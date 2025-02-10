89cd2159c2b07b98fa7970f508cf5e15e08ec9f3
ramiro <ramiro.sarasola@gmail.com>
2025-02-10 13:20:00 -0300

    Add new SVG icons for footer buttons and update button labels for improved clarity

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -6,55 +6,55 @@ export const Footer = () => {
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
       <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
         <Button size="lg" variant="ghost" className="w-full">
-          <Image 
-            src="/hr.svg" 
-            alt="Croatian" 
-            height={32} 
+          <Image
+            src="/thinking-person.svg"
+            alt="Croatian"
+            height={32}
             width={40}
             className="mr-4 rounded-md"
           />
-          Croatian
+          Logica
         </Button>
         <Button size="lg" variant="ghost" className="w-full">
-          <Image 
-            src="/es.svg" 
-            alt="Spanish" 
-            height={32} 
+          <Image
+            src="/light-bulb.svg"
+            alt="Flexibilidad Cognitiva"
+            height={32}
             width={40}
             className="mr-4 rounded-md"
           />
-          Spanish
+          Flexibilidad Cognitiva
         </Button>
         <Button size="lg" variant="ghost" className="w-full">
-          <Image 
-            src="/fr.svg" 
-            alt="French" 
-            height={32} 
+          <Image
+            src="/team-meeting.svg"
+            alt="Atentcion"
+            height={32}
             width={40}
             className="mr-4 rounded-md"
           />
-          French
+          Atencion
         </Button>
         <Button size="lg" variant="ghost" className="w-full">
-          <Image 
-            src="/it.svg" 
-            alt="Italian" 
-            height={32} 
+          <Image
+            src="/glowing-star.svg"
+            alt="Memoria"
+            height={32}
             width={40}
             className="mr-4 rounded-md"
           />
-          Italian
+          Memoria
         </Button>
-        <Button size="lg" variant="ghost" className="w-full">
-          <Image 
-            src="/jp.svg" 
-            alt="Japanese" 
-            height={32} 
+        {/* <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/jp.svg"
+            alt="Japanese"
+            height={32}
             width={40}
             className="mr-4 rounded-md"
           />
           Japanese
-        </Button>
+        </Button> */}
       </div>
     </footer>
   );
