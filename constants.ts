f7d57d4f443da81ed8839e1af9b19eff985f2374
ramiro <ramiro.sarasola@gmail.com>
2025-02-10 13:25:55 -0300

    Translate quest titles and button text to Spanish for improved localization

--- constants.ts
+++ constants.ts
@@ -2,23 +2,23 @@ export const POINTS_TO_REFILL = 10;
 
 export const quests = [
   {
-    title: "Earn 20 XP",
+    title: "Ganar 20 XP",
     value: 20,
   },
   {
-    title: "Earn 50 XP",
+    title: "Ganar 50 XP",
     value: 50,
   },
   {
-    title: "Earn 100 XP",
+    title: "Ganar 100 XP",
     value: 100,
   },
   {
-    title: "Earn 500 XP",
+    title: "Ganar 500 XP",
     value: 500,
   },
   {
-    title: "Earn 1000 XP",
+    title: "Ganar 1000 XP",
     value: 1000,
   },
 ];
