a21e418369d2f764cd25ed04eea19d154b52c4ac
ramiro <ramiro.sarasola@gmail.com>
2025-02-10 13:30:48 -0300

    Translate footer text to Spanish for improved localization

--- app/lesson/footer.tsx
+++ app/lesson/footer.tsx
@@ -11,41 +11,38 @@ type Props = {
   lessonId?: number;
 };
 
-export const Footer = ({
-  onCheck,
-  status,
-  disabled,
-  lessonId,
-}: Props) => {
+export const Footer = ({ onCheck, status, disabled, lessonId }: Props) => {
   useKey("Enter", onCheck, {}, [onCheck]);
   const isMobile = useMedia("(max-width: 1024px)");
 
   return (
-    <footer className={cn(
-      "lg:-h[140px] h-[100px] border-t-2",
-      status === "correct" && "border-transparent bg-green-100",
-      status === "wrong" && "border-transparent bg-rose-100",
-    )}>
+    <footer
+      className={cn(
+        "lg:-h[140px] h-[100px] border-t-2",
+        status === "correct" && "border-transparent bg-green-100",
+        status === "wrong" && "border-transparent bg-rose-100"
+      )}
+    >
       <div className="max-w-[1140px] h-full mx-auto flex items-center justify-between px-6 lg:px-10">
         {status === "correct" && (
           <div className="text-green-500 font-bold text-base lg:text-2xl flex items-center">
             <CheckCircle className="h-6 w-6 lg:h-10 lg:w-10 mr-4" />
-            Nicely done!
+            Buen trabajo!
           </div>
         )}
         {status === "wrong" && (
           <div className="text-rose-500 font-bold text-base lg:text-2xl flex items-center">
             <XCircle className="h-6 w-6 lg:h-10 lg:w-10 mr-4" />
-            Try again.
+            Prueba con otra respuesta.
           </div>
         )}
         {status === "completed" && (
           <Button
             variant="default"
             size={isMobile ? "sm" : "lg"}
-            onClick={() => window.location.href = `/lesson/${lessonId}`}
+            onClick={() => (window.location.href = `/lesson/${lessonId}`)}
           >
-            Practice again
+            Practicar de nuevo
           </Button>
         )}
         <Button
@@ -55,10 +52,10 @@ export const Footer = ({
           size={isMobile ? "sm" : "lg"}
           variant={status === "wrong" ? "danger" : "secondary"}
         >
-          {status === "none" && "Check"}
-          {status === "correct" && "Next"}
-          {status === "wrong" && "Retry"}
-          {status === "completed" && "Continue"}
+          {status === "none" && "Verificar"}
+          {status === "correct" && "Continuar"}
+          {status === "wrong" && "Reintentar"}
+          {status === "completed" && "Continuar"}
         </Button>
       </div>
     </footer>
