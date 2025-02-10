2f2ce76b51701a81feca4b0970010ac101cf0fdf
ramiro <ramiro.sarasola@gmail.com>
2025-02-10 13:33:55 -0300

    Translate exit modal text to Spanish for improved localization

--- components/modals/exit-modal.tsx
+++ components/modals/exit-modal.tsx
@@ -31,40 +31,35 @@ export const ExitModal = () => {
       <DialogContent className="max-w-md">
         <DialogHeader>
           <div className="flex items-center w-full justify-center mb-5">
-            <Image
-              src="/mascot_sad.svg"
-              alt="Mascot"
-              height={80}
-              width={80}
-            />
+            <Image src="/mascot_sad.svg" alt="Mascot" height={80} width={80} />
           </div>
           <DialogTitle className="text-center font-bold text-2xl">
-            Wait, don&apos;t go!
+            Espera, no te vayas!
           </DialogTitle>
           <DialogDescription className="text-center text-base">
-            You&apos;re about to leave the lesson. Are you sure?
+            Estas por abandonar la lección. Estas seguro?
           </DialogDescription>
         </DialogHeader>
         <DialogFooter className="mb-4">
           <div className="flex flex-col gap-y-4 w-full">
-            <Button 
-              variant="primary" 
-              className="w-full" 
-              size="lg" 
+            <Button
+              variant="primary"
+              className="w-full"
+              size="lg"
               onClick={close}
             >
-              Keep learning
+              Seguir aprendiendo
             </Button>
             <Button
-              variant="dangerOutline" 
-              className="w-full" 
-              size="lg" 
+              variant="dangerOutline"
+              className="w-full"
+              size="lg"
               onClick={() => {
                 close();
                 router.push("/learn");
               }}
             >
-              End session
+              Abandonar
             </Button>
           </div>
         </DialogFooter>
